1b246d691822f3e0219a46bff56a8b911e005c83
Saudamini0512 <Saudamini051294@gmail.com>
2021-05-09 16:42:33 +0530

    committing ifelse.py and modifying abstract.py file

--- abstract.py
+++ abstract.py
@@ -20,3 +20,4 @@ class Rectangle(Shape):
 rect1 = Rectangle()
 print(rect1.printarea())
 
+# this is a comment
---
+++ ifelse.py
@@ -0,0 +1,8 @@
+age= int(input("Enter your age"))
+if age<18:
+    print("You cannot driver")
+elif age==18:
+    print("we are not sure, come and give the physical test")
+
+else:
+    print("you can drive")
